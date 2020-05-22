d283dc39cad7e99c592b0d067f8cdd20e97875c4
mks-94 <muralikrishna9424@gmail.com>
2020-05-22 23:21:23 +0530

    Updates

---
+++ precache-manifest.4f40638b248c6aaf33eaee8f6ce333ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4277c1ace93a6e9435630b96c6ead2d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6f4d9dd1cb1ad36417a4",
+    "url": "/monster-rolodex/static/css/main.67fc209a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4d9dd1cb1ad36417a4",
+    "url": "/monster-rolodex/static/js/main.54882507.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
