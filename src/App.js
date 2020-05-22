4ac71f3be3958ec05ea7bb5c29add55d175f64e6
murali krishna <murali.krishna@girmiti.com>
2020-05-22 22:31:12 +0530

    monster-rolodex

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list-component";
+import { SearchBox } from "./components/search-box/search-box-component";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  // handleChange(e) {
+  //   this.setState({ searchField: e.target.value });
+  // }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+
+    console.log(monsters, "Monsters here");
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
